3bcb91f9717b673dc4208b3156dc7b091606c826
KarLOL82 <klinfeldt@gmail.com>
2023-01-10 20:07:53 -0800

    working connectivity checker

---
+++ python_app.py
@@ -0,0 +1,17 @@
+import urllib.request as urllib
+
+
+def main(url):
+    print("Checking connectivity ")
+
+    response = urllib.urlopen(url)
+    print("Connected to", url, "successfully")
+    print("The response code was: ", response.getcode())
+
+print("This is a site connectivity checker program")
+input_url = input("Input the url of the site you would like to check: ")
+
+main(input_url)
+
+
+    
